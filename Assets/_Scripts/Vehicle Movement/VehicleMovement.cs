99ec1c22eebb6cb609b7ec4819f8f0764a1994c3
Fabian Evers <f2abianski@gmail.com>
2026-01-06 13:33:42 +0100

    More options for vehicle movement
    - Decide if the vehicle stops, when there is no input
    - Set a start movement input

--- Assets/_Scripts/Vehicle Movement/VehicleMovement.cs
+++ Assets/_Scripts/Vehicle Movement/VehicleMovement.cs
@@ -9,16 +9,35 @@ public class VehicleMovement : MonoBehaviour
     [SerializeField, Min(0), Tooltip("x = forward, y = backwards")] private Vector2 motorSpeed = new(600f, 300f);
     [SerializeField, Tooltip("x = forward, y = backwards")] private Vector2 motorForce = new(25f, 25f);
 
+    [Header("Settings")]
+    [SerializeField, Tooltip("Decides if the vehicle stops, when the input direction (y) is 0")] private bool stopOnNoInput = true;
+    [SerializeField, Range(-1,1), Tooltip("Decides if the vehicle alredy starts with a certain move direction")] private int startMovementInput = 0;
+
+    void OnValidate()
+    {
+        if(startMovementInput != 0 && stopOnNoInput)
+            Debug.LogWarning("Having startMovementInput other than 0 only makes sens if you have stopOnNoInput disabled");
+    }
+
     private void Awake() {
         foreach(var h in hingeJoints)
             h.useMotor = true;
     }
-
+    void Start()
+    {
+        if(startMovementInput != 0)
+            SetInputDirection(new(0, startMovementInput));
+    }
     public void SetInputDirection(Vector2 inputDirection)
     {
+        // Handle velocity
         int newDirection = Mathf.RoundToInt(inputDirection.y);
-        float newTargetVelocity = (newDirection >= 0 ? motorSpeed.x : motorSpeed.y) * newDirection;
+        float newTargetVelocity = newDirection >= 0 ? motorSpeed.x : -motorSpeed.y;
         float newMotorForce = newDirection >= 0? motorForce.x : motorForce.y;
+
+        // Handle stoping the vehicle
+        if(newDirection == 0 && stopOnNoInput)
+            newTargetVelocity = 0;
 
         foreach(HingeJoint h in hingeJoints)
         {
