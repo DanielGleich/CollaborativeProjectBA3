09d9c020a586f8c92d32c3f5b9392d06e9a6197c
Fabian Evers <f2abianski@gmail.com>
2026-01-21 13:00:12 +0100

    Updated Cinemachine View Switch to offer methods for switching views themselve

--- Assets/_Scripts/Scientist/CinemachineViewSwitch.cs
+++ Assets/_Scripts/Scientist/CinemachineViewSwitch.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 
 /// <summary>
 /// Switches between multiple Cinemachine cameras by changing the priority 
-/// The "Switch Interaction" component is ideal for manually switching between views
 /// </summary>
 public class CinemachineViewSwitch : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private CinemachineCamera[] availableViews;
-    [SerializeField] private CinemachineCamera startView;
-    
+    [SerializeField, Tooltip("The game always starts at the first view [0]")] private CinemachineCamera[] availableViews;
+
+    [Header("Settings")]
+    [SerializeField] private SwitchType switchType;
+
+    private int currentIndex = 0;
+
     [ContextMenu("Get all avialable views in children")]
     private void GetViewsInChildren()
     {
         availableViews = GetComponentsInChildren<CinemachineCamera>(true);
     }
-    void Start()
-    {
-        if(startView)
-            SelectAvailableView(startView);
-    }
+    private void Awake() => SelectAvailableView(0);
     public void SelectAvailableView(int index)
     {
-        if (index <= availableViews.Length)
+        if (index >= availableViews.Length)
         {
             Debug.LogError("Out of range");
             return;
         }
+        currentIndex = index;
         SelectAvailableView(availableViews[index]);
     }
     public void SelectAvailableView(CinemachineCamera selectedView)
@@ -39,7 +39,29 @@ public class CinemachineViewSwitch : MonoBehaviour
             Debug.LogError("The selected Cinemachine Camera is not available");
             return;
         }
-        Array.ForEach(availableViews, x => x.Priority = 0);
-        selectedView.Priority = 1;
+        Array.ForEach(availableViews, x => x.Priority = x == selectedView ? 10 : 0);
+    }
+    public void SwitchView()
+    {
+        switch (switchType)
+        {
+            case SwitchType.Up:
+                currentIndex = (currentIndex + 1) % availableViews.Length;
+                break;
+
+            case SwitchType.Down:
+                currentIndex = (currentIndex - 1) % availableViews.Length;
+                break;
+
+            case SwitchType.Random:
+                currentIndex = UnityEngine.Random.Range(0, availableViews.Length);
+                break;
+        }
+        SelectAvailableView(currentIndex);
+    }
+
+    private enum SwitchType
+    {
+        Up, Down, Random
     }
 }
