d31dac796c00d96f28af1869803d83e0720f87a9
Daniel Gleich <gleichdaniel16@gmail.com>
2026-01-25 12:34:31 +0100

    Squashed commit of the following:
    commit 44166592c9ac7135e0b36747f123201e56d78ac0
    Merge: da2330a 70745d0
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 23:52:27 2026 +0100
    
        Merge branch 'daniel' into wip
    
    commit 70745d009d9d952dba3f382c62beed9e7cd1c2b7
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 23:50:48 2026 +0100
    
        Update PLR.prefab
    
    commit 99f121bd0cca6fdeda9390a17701d6b9e002e13f
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 23:47:29 2026 +0100
    
        Cleanup
    
    commit c9c48cfa1b09fd93dcfde5827931c51c195e38e4
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 22:34:06 2026 +0100
    
        Updated ClientDisconnectHandler
    
    commit 1d9f510aa47b97ad2e3dfd9072122619c91d1273
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 22:23:31 2026 +0100
    
        Maybe Last changes for GameEndScreen
    
    commit 0dcc70acc1e3e68e7377070eb5ae0058abdf007e
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 22:11:17 2026 +0100
    
        not so final GameEndScreen changes
    
    commit 1641fa368d3727e899c2e9077977d68cceda35ad
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 21:48:53 2026 +0100
    
        maybe last time changes
    
    commit fe433a944dad9fba00f802cb1c30c16c6274a5e5
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 21:24:13 2026 +0100
    
        Update EditorBuildSettings.asset
    
    commit b84376f31e10518cd1eae7c93590255aac6bad65
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 21:21:33 2026 +0100
    
        more small changes
    
        need to do small, so I can test with my laptop
    
    commit b9d49141c3fcc3e5892ba304ce264a5c0430071b
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 21:11:18 2026 +0100
    
        Next Adjustments for GameEndUI
    
    commit b07cab64b77a4163dc9a6f317d9c1fabdc62ce43
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 19:48:36 2026 +0100
    
        FishNet bugs are taking over
    
    commit 1b3998b65ad6ba9523d166e9d8036dc0c4d3772d
    Author: Daniel Gleich <gleichdaniel16@gmail.com>
    Date:   Sat Jan 24 18:01:45 2026 +0100
    
        First implementation of GameEndScreen

--- Assets/_Scripts/UI/GameEndScreen.cs
+++ Assets/_Scripts/UI/GameEndScreen.cs
@@ -1,14 +1,26 @@
 using FishNet;
+using FishNet.Connection;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameEndScreen : MonoBehaviour
+public class GameEndScreen : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] GameObject uiParent;
     [SerializeField] GameObject winnerUI;
     [SerializeField] GameObject loserUI;
+    [SerializeField] Button rematchButton;
+    [SerializeField] Button teamSelectionButton;
     [SerializeField] TextMeshProUGUI deathText;
+    [SerializeField] TextMeshProUGUI rematchButtonText;
+    [SerializeField] TextMeshProUGUI rematchCounter;
+
+    private readonly SyncList<NetworkConnection> playerReadyForRematch = new SyncList<NetworkConnection>();
+    private readonly SyncVar<bool> noPlayerLeft = new SyncVar<bool>();
 
     private void Awake()
     {
@@ -20,18 +32,77 @@ public class GameEndScreen : MonoBehaviour
         GameManager.OnGameOver.AddListener(ActivateScreen);
         GameManager.OnTeamWins.AddListener(HandleWinLoseScreen);
         PlayerManager.OnPlayerDisconnected.AddListener(BackupPlayerLeaveScreen);
+        noPlayerLeft.OnChange += NoPlayerLeft_OnChange;
     }
+
 
     private void OnDisable()
     {
         GameManager.OnGameOver.RemoveListener(ActivateScreen);
         GameManager.OnTeamWins.RemoveListener(HandleWinLoseScreen);
         PlayerManager.OnPlayerDisconnected.RemoveListener(BackupPlayerLeaveScreen);        
+        noPlayerLeft.OnChange -= NoPlayerLeft_OnChange;
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        noPlayerLeft.Value = true;
+        playerReadyForRematch.OnChange += PlayerReadyForRematch_OnChange;
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        GameManager.Instance.gameEndReason.OnChange += GameEndReason_OnChange;
+    }
+
+    private void GameEndReason_OnChange(GameEndReason prev, GameEndReason next, bool asServer)
+    {
+        if (uiParent.activeSelf && loserUI.activeSelf)
+        {
+            switch (GameManager.Instance?.gameEndReason.Value)
+            {
+                case GameEndReason.DESTROYED:
+                    deathText.text = "Your RattleBot got destroyed!";
+                    break;
+                case GameEndReason.DISCONNECT:
+                    deathText.text = "Your team mate left the game!";
+                    break;
+                case GameEndReason.OUTOFARENA:
+                    deathText.text = "Your RattleBot left the arena!";
+                    break;
+            }
+        }
+    }
+
+    private void NoPlayerLeft_OnChange(bool prev, bool next, bool asServer)
+    {
+        rematchButton.interactable = next;
+    }
+
+    [Server]
+    private void PlayerReadyForRematch_OnChange(SyncListOperation op, int index, NetworkConnection oldItem, NetworkConnection newItem, bool asServer)
+    {
+        if (noPlayerLeft.Value && playerReadyForRematch.Count == PlayerManager.Instance.AllPlayerConnections.Count)
+            NotifyRematch();
+        NotifyPlayerRematchReady();
+    }
+
+    [ObserversRpc]
+    private void NotifyPlayerRematchReady()
+    { 
+        rematchButtonText.text = playerReadyForRematch.Contains(LocalConnection) ? "Waiting for Rematch" : "Rematch?";
+        rematchCounter.text = $"({playerReadyForRematch.Count}/{PlayerManager.Instance.AllPlayerConnections.Count})";
     }
 
     private void ActivateScreen()
     {
         uiParent.SetActive(true);
+        teamSelectionButton.interactable = IsServerInitialized;
+        rematchCounter.text = $"({playerReadyForRematch.Count}/{PlayerManager.Instance.AllPlayerConnections.Count})";
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     private void HandleWinLoseScreen(int winnerTeamId)
@@ -61,10 +132,65 @@ public class GameEndScreen : MonoBehaviour
 
     private void BackupPlayerLeaveScreen(int clientId)
     {
+        RequestPlayerLeftServerRpc();
         if (deathText.text == string.Empty)
         { 
             loserUI.SetActive(true);
             deathText.text = "A player left the game";
         }
+    }
+
+    public void RequestRematch()
+    {
+        if (playerReadyForRematch.Contains(LocalConnection))
+            UntriggerRematch(LocalConnection);
+        else
+            TriggerRematch(LocalConnection);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void TriggerRematch(NetworkConnection c)
+    {
+        playerReadyForRematch.Add(c);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void UntriggerRematch(NetworkConnection c)
+    {
+        playerReadyForRematch.Remove(c);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestPlayerLeftServerRpc()
+    {
+        noPlayerLeft.Value = false;
+    }
+
+    [ObserversRpc]
+    private void NotifyRematch()
+    {
+        PlayerManager.Instance.ResetManagerForSceneChange();
+        TeamManager.Instance.ResetPlayerReadyStates();
+        NetworkSceneManager.LoadNetworkScene("Tutorial", new string[] { "Game" });
+    }
+
+    public void MoveToTeamSelection()
+    {
+        RequestPlayerLeftServerRpc();
+        if (playerReadyForRematch.Contains(LocalConnection))
+            UntriggerRematch(LocalConnection);
+
+        PlayerManager.Instance.ResetManagerForSceneChange();
+        TeamManager.Instance.ResetPlayerReadyStates();
+        NetworkSceneManager.LoadNetworkScene("ConnectingScene", new string[] { "Game" });
+    }
+
+    public void MoveToMainMenu()
+    {
+        if (InstanceFinder.IsServerStarted)
+            InstanceFinder.ServerManager.StopConnection(true);
+        InstanceFinder.ClientManager.StopConnection();
+        LobbyConnectionManager.LeaveLobby();
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ConnectingScene");
     }
 }
