9c6d25026cac9bdf6bdc191e239cf08816dbea06
Fabian Evers <f2abianski@gmail.com>
2026-01-25 13:57:11 +0100

    Update ExplosionForceRigidbody3d.cs
    - Offset can now be set
    - Explosion force can only be applied locally
    - Gizmos get displayed on Select instead of constantly

--- Assets/_Scripts/Misc/Physics/ExplosionForceRigidbody3d.cs
+++ Assets/_Scripts/Misc/Physics/ExplosionForceRigidbody3d.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 /// Knocks back objects with a Rigidbody attached if they are within a certain radius
 /// </summary>
 public class ExplosionForceRigidbody3d : MonoBehaviour {
+    [Header("Scale & Position Settings")]
+    [SerializeField] private Vector3 offset;
     [SerializeField, Min(0)] private float radius = 3;
+
+    [Header("Force Settings")]
     [SerializeField] private Vector2 forceRange = new Vector2(10, 0);
+    [SerializeField, Tooltip("Only applies explosion force to child objects")] private bool onlyLocal = false;
+
+    [Header("Trigger Settings")]
     [SerializeField] private bool triggerOnStart;
 
     void Start()
@@ -18,18 +25,21 @@ public class ExplosionForceRigidbody3d : MonoBehaviour {
         var colliders = Physics.OverlapSphere(transform.position, radius);
         foreach(Collider c in colliders)
         {
+            if(onlyLocal && !c.transform.IsChildOf(transform))
+                continue;
             if(c.attachedRigidbody)
             {
-                Vector3 direction = c.transform.position - transform.position;
+                Vector3 direction = c.transform.position - transform.position + offset;
                 float strength = Mathf.Lerp(forceRange.x, forceRange.y, direction.magnitude / radius);
                 c.attachedRigidbody.AddForce(direction.normalized * strength, ForceMode.Impulse);
             }
         }
     }
-
-    void OnDrawGizmos()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(transform.position + offset, radius);
+        Gizmos.DrawIcon(transform.position + offset, "Explosion Gizmo");
+        
     }
 }
