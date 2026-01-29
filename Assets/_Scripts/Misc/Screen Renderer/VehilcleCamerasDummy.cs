0305e53ff6f37b71b9a3141e65a8fcab8ec822d5
Fabian Evers <f2abianski@gmail.com>
2026-01-29 21:02:57 +0100

    Update VehilcleCamerasDummy.cs

--- Assets/_Scripts/Misc/Screen Renderer/VehilcleCamerasDummy.cs
+++ Assets/_Scripts/Misc/Screen Renderer/VehilcleCamerasDummy.cs
@@ -5,12 +5,14 @@ public class VehilcleCamerasDummy : NetworkedDummy
 {
     [HideInInspector] public VehicleCameras vehicleCamera;
     public event Action OnComponentFound;
+    public bool VehicleCamerasFound {get; private set;} = false;
     protected override void GetNetworkedComponent(GameObject other)
     {
         if(other.TryGetComponent<VehicleCameras>(out vehicleCamera))
         {
             Debug.Log("Vehicle Camera found");
             OnComponentFound?.Invoke();
+            VehicleCamerasFound = true;
         }
         else
             Debug.LogError("Component not found");
