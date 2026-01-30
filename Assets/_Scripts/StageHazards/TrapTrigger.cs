45e0eb7052612f906f412d2056df92656ff2cfba
Fabian Evers <f2abianski@gmail.com>
2026-01-30 14:20:48 +0100

    Hammer Tarp Implemented

---
+++ Assets/_Scripts/StageHazards/TrapTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrapTrigger : MonoBehaviour {
+    [Header("Settings")]
+    [SerializeField, Tooltip("Has to be set bevore play mode is entered")] private bool triggerOnStageHazardTriggered;
+    [SerializeField] private UnityEvent onTriggered;
+    [SerializeField, Min(0)] private float triggerCooldown = 2f;
+
+    private float lastTriggerTimeStamp;
+
+    void Awake()
+    {
+        lastTriggerTimeStamp = -triggerCooldown;
+    }
+
+    void OnEnable()
+    {
+        if(triggerOnStageHazardTriggered)
+            StageHazard.OnTriggered += Trigger;
+    }
+    void OnDisable()
+    {
+        if(triggerOnStageHazardTriggered)
+            StageHazard.OnTriggered -= Trigger;
+    }
+    [ContextMenu("Trigger Weapon")]
+    public void Trigger()
+    {
+        if(Time.time - triggerCooldown < lastTriggerTimeStamp)
+        {
+            Debug.LogWarning("Weapon can't be triggerd because the cooldown is still active");
+            return;
+        }
+        onTriggered?.Invoke();
+        lastTriggerTimeStamp = Time.time;
+    }
+}
