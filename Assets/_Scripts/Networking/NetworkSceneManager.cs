14d4bb61f91eb3acb64003b84a75465d2e69462c
Daniel Gleich <gleichdaniel16@gmail.com>
2026-02-01 00:11:43 +0100

    Update NetworkSceneManager.cs

--- Assets/_Scripts/Networking/NetworkSceneManager.cs
+++ Assets/_Scripts/Networking/NetworkSceneManager.cs
@@ -5,15 +5,30 @@ using System.Linq;
 
 public static class NetworkSceneManager
 {
-    public static void LoadNetworkScene(string sceneToLoad, string[] scenesToUnload, bool replaceOption = true)
+    //public static void LoadNetworkScene(string sceneToLoad, string[] scenesToUnload, bool replaceOption = true)
+    //{
+    //    if (!InstanceFinder.IsServerStarted) { return; }
+
+    //    SceneLoadData sceneLoadData = replaceOption ? new SceneLoadData(sceneToLoad) { ReplaceScenes = ReplaceOption.All } : new SceneLoadData(sceneToLoad);
+    //    NetworkConnection[] connections = InstanceFinder.ServerManager.Clients.Values.ToArray();
+    //    InstanceFinder.SceneManager.LoadGlobalScenes(sceneLoadData);
+
+    //    if (scenesToUnload == null) return;
+    //    foreach (string sceneName in scenesToUnload)
+    //    {
+    //        SceneUnloadData sceneUnloadData = new SceneUnloadData(sceneName);
+    //        InstanceFinder.SceneManager.UnloadGlobalScenes(sceneUnloadData);
+    //    }
+    //}
+
+    public static void LoadNetworkScene(string sceneToLoad, string[] scenesToUnload)
     {
         if (!InstanceFinder.IsServerStarted) { return; }
 
-        SceneLoadData sceneLoadData = replaceOption ? new SceneLoadData(sceneToLoad) { ReplaceScenes = ReplaceOption.All } : new SceneLoadData(sceneToLoad);
+        SceneLoadData sceneLoadData = new SceneLoadData(sceneToLoad);
         NetworkConnection[] connections = InstanceFinder.ServerManager.Clients.Values.ToArray();
         InstanceFinder.SceneManager.LoadGlobalScenes(sceneLoadData);
 
-        if (scenesToUnload == null) return;
         foreach (string sceneName in scenesToUnload)
         {
             SceneUnloadData sceneUnloadData = new SceneUnloadData(sceneName);
