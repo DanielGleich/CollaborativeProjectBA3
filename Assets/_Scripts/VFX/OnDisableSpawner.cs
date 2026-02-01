ecd809ec9edecd254cbd359daf4333c4708d5e31
Daniel Gleich <gleichdaniel16@gmail.com>
2026-02-01 22:40:16 +0100

    fixed DeathFeedback Script

--- Assets/_Scripts/VFX/OnDisableSpawner.cs
+++ Assets/_Scripts/VFX/OnDisableSpawner.cs
@@ -1,12 +1,43 @@
+using FishNet.Connection;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class OnDisableSpawner : MonoBehaviour
+public class OnDeathSpawner : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] HealthNetworking networkedHealth;
     [SerializeField] GameObject prefab;
 
+    [Header("Settings")]
+    [SerializeField] bool triggerOnce = true;
+
+    bool isTriggered = false;
+
+    private void OnEnable()
+    {
+        //if (localHealth != null)
+        //    localHealth.OnDeath += Spawn;
+        if (networkedHealth != null)
+            networkedHealth.OnNetworkedDeath += Spawn;
+    }
     private void OnDisable()
     {
-        Instantiate(prefab, gameObject.transform.position, Quaternion.identity);        
+        //if (localHealth != null)
+        //    localHealth.OnDeath -= Spawn;
+        if (networkedHealth != null)
+            networkedHealth.OnNetworkedDeath -= Spawn;
+    }
+
+    private void Spawn()
+    {
+        Debug.Log("A");
+        if (triggerOnce && isTriggered) return;
+        Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+        isTriggered = true;
+    }
+
+    private void Spawn(NetworkConnection c)
+    {
+        Spawn();
     }
 }
