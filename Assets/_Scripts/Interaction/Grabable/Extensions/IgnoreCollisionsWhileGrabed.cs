53ac2637fc603353e5707d4fe7911f47496d60e5
Fabian Evers <f2abianski@gmail.com>
2026-02-01 15:38:32 +0100

    Updated Grabbing Mechanic
    - Separated Grabbable3d & Dropping grabbles on collision into 2 components (CollisionForceReaction3d)
    - Added a component that allows to stop the character and the grabbed object to collide

---
+++ Assets/_Scripts/Interaction/Grabable/Extensions/IgnoreCollisionsWhileGrabed.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// Stops grabables from colliding with the player while beeing grabed
+/// </summary>
+public class IgnoreCollisionsWhileGrabed : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PickUpGrabable3d pickUpGrabable3D;
+    [SerializeField] private CharacterController characterController;
+
+    private Grabable3d currentGrabable;
+
+    void OnValidate()
+    {
+        if(!pickUpGrabable3D)
+            pickUpGrabable3D = GetComponent<PickUpGrabable3d>();
+        if(!characterController)
+            characterController = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        pickUpGrabable3D.OnPickUp += UpdateCurrentGrabable;
+    }
+    void OnDisable()
+    {
+        pickUpGrabable3D.OnPickUp -= UpdateCurrentGrabable;
+    }
+
+    private void UpdateCurrentGrabable(Grabable3d newGrabable)
+    {
+        if (currentGrabable && newGrabable != currentGrabable)
+            Physics.IgnoreCollision(currentGrabable.Collider, characterController, false);
+        if (newGrabable)
+        {
+            Physics.IgnoreCollision(newGrabable.Collider, characterController, true);
+            currentGrabable = newGrabable;
+        }
+    }
+}
