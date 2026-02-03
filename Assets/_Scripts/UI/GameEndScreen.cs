c84f885ca872c7c9dad5782d3dfa7eacf3059138
BobSpeedo <lujohardt@gmail.com>
2026-02-03 15:01:57 +0100

    Revert "ui implementation wip"
    This reverts commit 1aa1672957fde6ca83d49a0f1f14b419cd8a7598.

--- Assets/_Scripts/UI/GameEndScreen.cs
+++ Assets/_Scripts/UI/GameEndScreen.cs
@@ -4,26 +4,32 @@ using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class GameEndScreen : NetworkBehaviour
 {
     [Header("References")]
+    [SerializeField] GameObject uiParent;
+    [SerializeField] GameObject winnerUI;
+    [SerializeField] GameObject loserUI;
     [SerializeField] Button rematchButton;
     [SerializeField] Button teamSelectionButton;
+    [SerializeField] TextMeshProUGUI deathText;
     [SerializeField] TextMeshProUGUI rematchButtonText;
+    [SerializeField] TextMeshProUGUI rematchCounter;
 
     [Header("Settings")]
     [SerializeField] float OnDeathDelay = 3;
 
-    [Header("Events")]
-    public UnityEvent OnPlayerWin = new UnityEvent();
-    public UnityEvent OnPlayerLose = new UnityEvent();
-
     private readonly SyncList<NetworkConnection> playerReadyForRematch = new SyncList<NetworkConnection>();
     private readonly SyncVar<bool> noPlayerLeft = new SyncVar<bool>();
+
+    private void Awake()
+    {
+        uiParent.SetActive(false);
+    }
 
     private void OnEnable()
     {        
@@ -32,6 +38,7 @@ public class GameEndScreen : NetworkBehaviour
         PlayerManager.OnPlayerDisconnected.AddListener(BackupPlayerLeaveScreen);
         noPlayerLeft.OnChange += NoPlayerLeft_OnChange;
     }
+
 
     private void OnDisable()
     {
@@ -56,7 +63,21 @@ public class GameEndScreen : NetworkBehaviour
 
     private void GameEndReason_OnChange(GameEndReason prev, GameEndReason next, bool asServer)
     {
-        OnPlayerLose?.Invoke();
+        if (uiParent.activeSelf && loserUI.activeSelf)
+        {
+            switch (GameManager.Instance?.gameEndReason.Value)
+            {
+                case GameEndReason.DESTROYED:
+                    deathText.text = "Your RattleBot got destroyed!";
+                    break;
+                case GameEndReason.DISCONNECT:
+                    deathText.text = "Your team mate left the game!";
+                    break;
+                case GameEndReason.OUTOFARENA:
+                    deathText.text = "Your RattleBot left the arena!";
+                    break;
+            }
+        }
     }
 
     private void NoPlayerLeft_OnChange(bool prev, bool next, bool asServer)
@@ -78,7 +99,8 @@ public class GameEndScreen : NetworkBehaviour
     [ObserversRpc]
     private void NotifyPlayerRematchReady()
     { 
-        rematchButtonText.text = $"Rematch? ({playerReadyForRematch.Count}/{PlayerManager.Instance.AllPlayerConnections.Count})";
+        rematchButtonText.text = playerReadyForRematch.Contains(LocalConnection) ? "Waiting for Rematch" : "Rematch?";
+        rematchCounter.text = $"({playerReadyForRematch.Count}/{PlayerManager.Instance.AllPlayerConnections.Count})";
     }
 
     private void ActivateScreen()
@@ -89,8 +111,9 @@ public class GameEndScreen : NetworkBehaviour
     IEnumerator OnEnableDelay()
     {
         yield return new WaitForSeconds(OnDeathDelay);
+        uiParent.SetActive(true);
         teamSelectionButton.interactable = IsServerInitialized;
-        rematchButtonText.text = $"Rematch? (0/{PlayerManager.Instance.AllPlayerConnections.Count})";
+        rematchCounter.text = $"({playerReadyForRematch.Count}/{PlayerManager.Instance.AllPlayerConnections.Count})";
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
@@ -100,21 +123,34 @@ public class GameEndScreen : NetworkBehaviour
         int localClientId = InstanceFinder.ClientManager.Connection.ClientId;
         if (winnerTeamId == TeamMember.localTeamId)
         {
-            OnPlayerWin?.Invoke();
-            Debug.Log("Player w");
+            winnerUI.SetActive(true);
         }
         else
-        {
-            OnPlayerLose?.Invoke();
-            Debug.Log("Player l");
+        { 
+            loserUI.SetActive(true);
+            switch (GameManager.Instance?.gameEndReason.Value)
+            {
+                case GameEndReason.DESTROYED:
+                    deathText.text = "Your RattleBot got destroyed!";
+                break;
+                case GameEndReason.DISCONNECT:
+                    deathText.text = "Your team mate left the game!";
+                break;
+                case GameEndReason.OUTOFARENA:
+                    deathText.text = "Your RattleBot left the arena!";
+                break;
+            }
         }
     }
 
     private void BackupPlayerLeaveScreen(int clientId)
     {
         RequestPlayerLeftServerRpc();
-        OnPlayerLose?.Invoke();
-        Debug.Log("Player l2");
+        if (deathText.text == string.Empty)
+        { 
+            loserUI.SetActive(true);
+            deathText.text = "A player left the game";
+        }
     }
 
     public void RequestRematch()
