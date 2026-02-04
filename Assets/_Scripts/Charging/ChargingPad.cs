860e75e88f197e3dd8fa068fbb670807673aa273
Daniel Gleich <gleichdaniel16@gmail.com>
2026-02-04 18:00:12 +0100

    Revert "Update ChargingPad.cs"
    This reverts commit de5163759ed051adc0c8c76c5ce2827f6771b131.

--- Assets/_Scripts/Charging/ChargingPad.cs
+++ Assets/_Scripts/Charging/ChargingPad.cs
@@ -83,7 +83,7 @@ public class ChargingPad : NetworkBehaviour
             OverchargedStatus[] chargingStatuses = obj.GetComponentsInChildren<OverchargedStatus>();
             foreach (OverchargedStatus chargingStatus in chargingStatuses)
             {
-                if (chargingStatus.IsOvercharged == false)
+                if (chargingStatus.IsOvercharged.Value == false)
                 {
                     NotifyOverchargeRequest(chargingStatus.GetComponent<NetworkObject>());
                 }
