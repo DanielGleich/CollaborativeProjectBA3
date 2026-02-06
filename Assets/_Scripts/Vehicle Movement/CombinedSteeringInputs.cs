1467d4f8b7ae186ccf43b14fc068219bd94613ce
Fabian Evers <f2abianski@gmail.com>
2026-02-06 09:37:24 +0100

    Renamed Vehicle Steering Controls to Combined Steering Inputs

--- Assets/_Scripts/Vehicle Movement/CombinedSteeringInputs.cs
+++ Assets/_Scripts/Vehicle Movement/CombinedSteeringInputs.cs
@@ -1,18 +1,22 @@
 using System;
 using UnityEngine;
 
-public class VehicleSteeringControls : MonoBehaviour {
+/// <summary>
+/// Combines scientist inputs & ballast scale inputs based on a influence value
+/// </summary>
+public class CombinedSteeringInputs : MonoBehaviour
+{
     [Header("References")]
     [SerializeField] private NetworkedBallastScaleDummy ballastScale;
     [SerializeField] private VehicleSteering vehicleSteering;
 
-    [Header ("Settings")]
+    [Header("Settings")]
     [SerializeField] private float weigthBalanceMultiplier = -1;
-    [SerializeField, Range(0,1)] private float ballastScaleInfluence = .25f;
+    [SerializeField, Range(0, 1)] private float ballastScaleInfluence = .25f;
     [SerializeField, Min(0.001f), Tooltip("The amount of weight needed for the maximum steering power")] private float maxSteeringPowerWeight = 10f;
 
-    private float weightBalance;
-    private float inputDirection;
+    public float WeightBalance { get; private set; }
+    public float InputDirection { get; private set; }
 
     void OnEnable()
     {
@@ -24,17 +28,18 @@ public class VehicleSteeringControls : MonoBehaviour {
     }
     private void UpdateWeightBalance(Vector2 vector)
     {
-        weightBalance = vector.x;
+        WeightBalance = vector.x;
         UpdateSteering();
     }
     public void SetInputDirection(Vector2 inputDirection)
     {
-        this.inputDirection = Mathf.Round(inputDirection.x);
+        this.InputDirection = Mathf.Round(inputDirection.x);
         UpdateSteering();
     }
     public void UpdateSteering()
     {
-        float steeringInputX = inputDirection * (1 - ballastScaleInfluence) + Mathf.Clamp(weightBalance * weigthBalanceMultiplier / maxSteeringPowerWeight,-1,1) * ballastScaleInfluence;
-        vehicleSteering.SetInputDirection(new(steeringInputX,0));
+        float steeringInputX = InputDirection * (1 - ballastScaleInfluence) 
+            + Mathf.Clamp(WeightBalance * weigthBalanceMultiplier / maxSteeringPowerWeight, -1, 1) * ballastScaleInfluence;
+        vehicleSteering.SetInputDirection(new(steeringInputX, 0));
     }
 }
